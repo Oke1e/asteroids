448eff00ee57dfe0c59d8b46465a52e4706f784a
Okele <chris.milhon@gmail.com>
2025-01-24 16:25:00 -0700

    Asteroids day 1

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
+    #Setup
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
+
+    #Create game objects
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    #Game Loop
     try:
         while True:
+            dt = clock.tick(60) / 1000
+
+            #Handle events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     return
-          
-            screen.fill((0,0,0))
 
+            player.update(dt)
+
+            #Draw
+            screen.fill((0, 0, 0))
+            player.draw(screen)
             pygame.display.flip()
+    
     except KeyboardInterrupt:
         print("\nExiting game loop. Goodbye!")
 
